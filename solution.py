50394015ce5910a098e3d89321d2cced5861b9d0
fayco226 <faicallengane55@gmail.com>
2022-12-07 10:14:11 +0000

    save

--- solution.py
+++ solution.py
@@ -4,6 +4,7 @@ if len(string) > 10:
     print("string too long")
 elif len(string) < 10:
     print("string not long enough")
+print("first caractere is ", string[0],"\n last caractere is ", string[-1])
 result = ""
 for i in string:
     result = result + i
